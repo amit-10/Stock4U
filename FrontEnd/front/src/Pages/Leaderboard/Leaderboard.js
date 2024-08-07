7d5bb47e5bda83e5afc54954eba8f227546db302
sulimani55 <tktur9@gmail.com>
2024-08-07 13:59:08 +0300

    update rank

--- FrontEnd/front/src/Pages/Leaderboard/Leaderboard.js
+++ FrontEnd/front/src/Pages/Leaderboard/Leaderboard.js
@@ -97,7 +97,7 @@ function Leaderboard() {
                         <Box sx={{ display: 'flex', flexDirection: 'column' }}>
                             <CardContent sx={{ flex: '1 0 auto' }}>
                             <Typography component="div" variant="h5">
-                                Position
+                                Rank
                             </Typography>
                             <Typography variant="subtitle1" color="#405D72" component="div">
                                 {position}#
